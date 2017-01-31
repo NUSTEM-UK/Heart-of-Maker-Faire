53562907dc8a89e607b02011af860cce418a028d
Jonathan Sanderson <lists@quernstone.com>
2017-01-31 17:48:34 +0000

    Working heartbeat simulation code
    Generatest a simulated heartbeat with added noise, in numpy.

--- Heartsim/ecg_simulation/ecg_simulation.py
+++ Heartsim/ecg_simulation/ecg_simulation.py
@@ -1,17 +1,27 @@
+"""ECG simulator example code.
+
+Based on work in http://stackoverflow.com/questions/4387878/
+by Diarmaid O Cualain and Steve Tjoa.
+Published here: https://github.com/diarmaidocualain/ecg_simulation
+Licensed under BSD.
+Minor modifications by Jonathan Sanderson, Northumbria University, 2017-01-31
+"""
 import pylab
 import scipy.signal as signal
-import numpy 
+import numpy
 
 print('Simulating heart ecg')
 
 # The "Daubechies" wavelet is a rough approximation to a real,
 # single, heart beat ("pqrst") signal
+# Ref: https://en.wikipedia.org/wiki/Daubechies_wavelet
+# Ref: https://en.wikipedia.org/wiki/Ingrid_Daubechies
 pqrst = signal.wavelets.daub(10)
 
-# Add the gap after the pqrst when the heart is resting. 
+# Add the gap after the pqrst when the heart is resting.
 samples_rest = 10
 zero_array = numpy.zeros(samples_rest, dtype=float)
-pqrst_full = numpy.concatenate([pqrst,zero_array])
+pqrst_full = numpy.concatenate([pqrst, zero_array])
 
 # Plot the heart signal template
 pylab.plot(pqrst_full)
@@ -21,19 +31,19 @@ pylab.title('Heart beat signal Template')
 pylab.show()
 
 # Simulated Beats per minute rate
-# For a health, athletic, person, 60 is resting, 180 is intensive exercising
+# For a healthy, athletic person, 60 is resting, 180 is intensive exercising
 bpm = 60
 bps = bpm / 60
 
 # Simumated period of time in seconds that the ecg is captured in
 capture_length = 10
 
-# Caculate the number of beats in capture time period 
+# Caculate the number of beats in capture time period
 # Round the number to simplify things
 num_heart_beats = int(capture_length * bps)
 
 # Concatonate together the number of heart beats needed
-ecg_template = numpy.tile(pqrst_full , num_heart_beats)
+ecg_template = numpy.tile(pqrst_full, num_heart_beats)
 
 # Plot the heart ECG template
 pylab.plot(ecg_template)
@@ -42,7 +52,7 @@ pylab.ylabel('Amplitude (normalised)')
 pylab.title('Heart ECG Template')
 pylab.show()
 
-# Add random (gaussian distributed) noise 
+# Add random (gaussian distributed) noise
 noise = numpy.random.normal(0, 0.01, len(ecg_template))
 ecg_template_noisy = noise + ecg_template
 
@@ -55,23 +65,25 @@ pylab.show()
 
 
 # Simulate an ADC by sampling the noisy ecg template to produce the values
-# Might be worth checking nyquist here 
+# Might be worth checking nyquist here
 # e.g. sampling rate >= (2 * template sampling rate)
 sampling_rate = 50.0
-num_samples = sampling_rate * capture_length
+num_samples = int(sampling_rate * capture_length)
 ecg_sampled = signal.resample(ecg_template_noisy, num_samples)
 
-# Scale the normalised amplitude of the sampled ecg to whatever the ADC 
+# Scale the normalised amplitude of the sampled ecg to whatever the ADC
 # bit resolution is
-# note: check if this is correct: not sure if there should be negative bit values. 
+# note: check if this is correct: not sure if there should be
+# negative bit values.
 adc_bit_resolution = 1024
-ecg =  adc_bit_resolution * ecg_sampled
+ecg = adc_bit_resolution * ecg_sampled
 
 # Plot the sampled ecg signal
 pylab.plot(ecg)
 pylab.xlabel('Sample number')
 pylab.ylabel('bit value')
-pylab.title('%d bpm ECG signal with gaussian noise sampled at %d Hz' %(bpm, sampling_rate) )
+pylab.title('%d bpm ECG signal with gaussian noise sampled at %d Hz'
+            % (bpm, sampling_rate))
 pylab.show()
 
 print('saving ecg values to file')
