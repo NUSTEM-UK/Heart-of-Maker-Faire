2c96f5cd45bcddf5e170daa68891c8f523315b52
Joe <joeshimwell@gmail.com>
2017-02-28 14:06:59 +0000

    More printer testing to explore dimensions of qrcode printing

--- QRmaker/qrdabble.py
+++ QRmaker/qrdabble.py
@@ -1,12 +1,12 @@
 import qrcode
 qr = qrcode.QRCode(
     version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=30,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
     border=0,
 )
 qr.add_data(1)
 qr.make(fit=True)
 
 img = qr.make_image()
-img.save("output2.png")
+img.save("another.png")
