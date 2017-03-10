5f2c9157fc895ce2d75e7093718b43eae082aeb0
llewmihs <joeshimwell@gmail.com>
2017-03-10 15:00:32 +0000

    Flashy flashy worky worky

--- ScanPi/homf_neopixels.py
+++ ScanPi/homf_neopixels.py
@@ -11,19 +11,31 @@ LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
 # get the initial programme start time for our delay-less Neopixel update
-init_time = int(round(time.time()*1000))
-
+last_time_checked = int(round(time.time()*1000))
+frame = 0
 def millis():
     millis = int(round(time.time()*1000))
     return millis
 
-def get_frame(time0):
-    prog_time = millis() - time0
-    # the pulse cycle is 1 second, so find out what fraction we are through that cycle
-    sec_time = prog_time % 1000
-    # there are 240 diffrent sbrightness settings in the cycle, work out which one we're on
-    frame_count = sec_time % 240
-    return frame_count
+def pulselight(lasttime, count):
+    for i in range(LED_COUNT):
+        strip.setPixelColor(i, Color(255, 0, 0))
+    current_time = millis()
+    #what is the elapsed time
+    elap_time = current_time - lasttime
+    # pulse length is 1 second
+    pulse_length = 2
+    total_frames = 240
+    frame_rate = total_frames / pulse_length
+    # multiply the frame rate by the elapsed time accounting for millis
+    frames_passed = float(frame_rate * elap_time /1000)
+    new_count = count + frames_passed
+    time.sleep(0.01)
+    if new_count >=240:
+        new_count = 0
+    strip.setBrightness(frames[int(frame)])
+    strip.show()
+    return current_time, new_count
 
 
 if __name__ == '__main__':
@@ -32,13 +44,9 @@ if __name__ == '__main__':
         strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
         # Intialize the library (must be called once before other functions).
         strip.begin()
-        for i in range(LED_COUNT):
-            strip.setPixelColor(i, Color(255, 0, 0))
-
         while True:
-            count = get_frame(init_time)
-            strip.setBrightness(frames[count])
-            strip.show()
+            last_time_checked, frame = pulselight(last_time_checked, frame)
+
 
     except KeyboardInterrupt:
         for i in range(LED_COUNT):
