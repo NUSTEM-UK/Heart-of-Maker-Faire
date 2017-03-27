476859ead7e1b36bb1aab101b102287da84fa4a6
Llewmihs <joeshimwell@gmail.com>
2017-03-27 16:31:23 +0000

    Testing the real scanner a bit at a time

--- experimentation/rotaryEncoder/ledcheck.py
+++ experimentation/rotaryEncoder/ledcheck.py
@@ -1,50 +1,74 @@
 from RPi import GPIO
 from time import sleep
+from microdotphat import *
 
-from gpiozero import LED
+from gpiozero import LED, Button
 
-red = LED(21, active_high=False)
-blue = LED(22, active_high=False)
-green = LED(20, active_high=False)
+green = LED(8, active_high=False)
+red = LED(11, active_high=False)
+blue = LED(10, active_high=False)
 
-clk = 18
+
+greenB = LED(5, active_high=False)
+redB = LED(6, active_high=False)
+blueB = LED(13, active_high=False)
+
+button = Button(26)
+
+clk = 27
 dt = 17
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-counter = 0
-clkLastState = GPIO.input(clk)
 
-try:
 
-        while True:
-                clkState = GPIO.input(clk)
-                dtState = GPIO.input(dt)
-                if clkState != clkLastState:
-                        if dtState != clkState:
-                                counter += 1
-                        else:
-                                counter -= 1
-                        print counter/2
-                if counter/2 < 10:
-                    red.on()
-                    green.off()
-                    blue.off()
-                elif 10 <= counter/2 < 20:
-                    red.off()
-                    green.on()
-                    blue.off()
-                elif 20 <= counter/2 < 30:
-                    red.off()
-                    green.off()
-                    blue.on()
-                else:
-                    red.off()
-                    green.off()
-                    blue.off()
-                clkLastState = clkState
-                sleep(0.001)
-finally:
-        GPIO.cleanup()
+def encoder(colour):
+    red.on()
+    blue.off()
+    green.off()
+    if colour == 'magenta':
+        redB.on()
+        blueB.on()
+        greenB.off()
+    elif colour == 'yellow':
+        redB.on()
+        blueB.off()
+        greenB.on()
+    elif colour == 'cyan':
+        redB.on()
+        blueB.off()
+        greenB.on()
+    elif colour == 'green':
+        redB.on()
+        blueB.off()
+        greenB.on()
+    else:
+        redB.on()
+        blueB.off()
+        greenB.on()
+    counter = 40
+    clkLastState = GPIO.input(clk)
+    while True:
+        clear()
+        clkState = GPIO.input(clk)
+        dtState = GPIO.input(dt)
+        if clkState != clkLastState:
+            if dtState != clkState:
+                counter += 1
+            else:
+                counter -= 1
+            countString = " " + str(counter/2)
+            print(countString)
+            write_string(countString, kerning=False)
+            show()
+
+        clkLastState = clkState
+        sleep(0.001)
+        if button.is_pressed:
+            print('Button pressed')
+            return True
+
+if __name__ == '__main__':
+    encoder('magenta')
--- experimentation/rotaryEncoder/rotaryhomf.py
+++ experimentation/rotaryEncoder/rotaryhomf.py
@@ -2,34 +2,34 @@ from RPi import GPIO
 from gpiozero import Button
 from time import sleep, time
 from microdotphat import write_string, set_decimal, clear, show
-from neoshomf import *
+#from neoshomf import *
 
-clkPin = 17
-dtPin = 18
+clkPin = 27
+dtPin = 17
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(clkPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dtPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-button = Button(8)
+#button = Button(8)
 
-def setEncoderColour(colour):
-    if colour == 'cyan':
-        #set colour to blue
-    elif colour == 'yellow':
-
-    elif colour == '???'
-
-    else:
+#def setEncoderColour(colour):
+ #   if colour == 'cyan':
+   #     #set colour to blue
+  #  elif colour == 'yellow':
+#
+  #  elif colour == '???'
+#
+#    else:
 
 
 def heartEncoder():
     counter = 40
     clkLastState = GPIO.input(clkPin)
-    clear()
-    last_time_checked = int(round(time.time()*1000))
+    #clear()
+    #last_time_checked = int(round(time.time()*1000))
     while True:
-        last_time_checked, frame = pulselight(strip, last_time_checked, frame, 60/counter) # Can I make the neopixel ring pulse with HR
+        #last_time_checked, frame = pulselight(strip, last_time_checked, frame, 60/counter) # Can I make the neopixel ring pulse with HR
         clkState = GPIO.input(clkPin)
         dtState = GPIO.input(dtPin)
         if clkState != clkLastState:
@@ -37,10 +37,12 @@ def heartEncoder():
                 counter += 1
             else:
                 counter -= 1
-        print(counter)
         clkLastState = clkState
-        write_string(str(counter), kerning = False)
+        write_string(str(int(counter/2)), kerning = False)
         show()
-        if button.is_pressed():
-            # do a triple flash
-            return counter
+        #if button.is_pressed():
+         #   # do a triple flash
+         #   return counter
+        #sleep(0.001)
+while True:
+    heartEncoder()
